a7dba2fd9ef9f19b59c32ee0df9ec6cf85f61da3
Jakub Olejník <olejnjak@gmail.com>
2017-09-10 00:08:55 +0200

    Setup default appearance

--- KartingCoach/AppDelegate.swift
+++ KartingCoach/AppDelegate.swift
@@ -14,14 +14,20 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
 
+    // MARK: UIApplication delegate
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        
-        LapTime(duration: 10)
-        
+        setupAppearance()
         appFlow.start()
         return true
     }
 
+    // MARK: Private helpers
+    
+    private func setupAppearance() {
+        let tableView = UITableView.appearance()
+        tableView.tableFooterView = UIView()
+    }
 }
